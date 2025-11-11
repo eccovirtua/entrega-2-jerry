f1bc282a8a2dff8778a32ede5c4fbdc0d5b8bb34
Marcelo890 <37416206+Marcelo890@users.noreply.github.com>
2025-11-11 18:30:15 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import authRutas from './routes/auth.js';
 import practicasRouter from './routes/practicas.js';
 import reporteRutas from './routes/reporteRutas.js'; //importar rutas de reportes
+import documentosRutas from './routes/documentos.js'; //importar rutas de documentos
 
 const app = express();
 
@@ -18,5 +19,7 @@ app.use(express.json()); // Permite a Express entender JSON
 app.use('/api/auth', authRutas);
 app.use('/api', reporteRutas);
 app.use('/api/practicas', practicasRouter);
+app.use('/api/documentos', documentosRutas); //usar rutas de documentos
+
 
 export default app;
